2b8c8c92382db2198147bbc590a6b92d8d5c2225
Aleksandr Kasianov <64069598+nerzul88@users.noreply.github.com>
2020-06-01 14:34:34 +0300

    Add files via upload

---
+++ ITMO.Labs.WinForms.01Intro/ITMO.Labs.WinForms.01Intro.Task5.1/OvalForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.Labs.WinForms._01Intro.Task5._1
+{
+    public partial class OvalForm : Form
+    {
+        public OvalForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OvalForm_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddEllipse(0, 0, this.Width, this.Height);
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
