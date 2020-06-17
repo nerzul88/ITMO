63d14d961776f1516c44a555b23865d9e76b1333
Aleksandr Kasianov <64069598+nerzul88@users.noreply.github.com>
2020-06-17 19:08:06 +0300

    Add files via upload

---
+++ WindowsForms/ITMO.Labs.WinForms.01Intro/ITMO.Labs.WinForms.01Intro.Task3/nForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ITMO.Labs.WinForms._01Intro.Task3
+{
+    public partial class nForm : ITMO.Labs.WinForms._01Intro.Task3.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsForms/ITMO.Labs.WinForms.01Intro/ITMO.Labs.WinForms.01Intro.Task5.2/FirstForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.Labs.WinForms._01Intro.Task5._2
+{
+    public partial class FirstForm : Form
+    {
+        public FirstForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FirstForm_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddLines(new[] { new Point(0, Height / 2), new Point(Width / 2, 0), new Point(Width, Height / 2), new Point(Width / 2, Height) });
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+        }
+    }
+}
